156dd44beb22721920d4fac6b6859d9fdcecb068
Evandrolds <evandro.lds.ds@outlook.com>
2022-11-25 11:41:32 -0300

    Criated Conta Entity

---
+++ Conta Bancaria  CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Conta_Bancaria;
+
+class Conta {
+
+    private string _holder;
+    public long Number { get; private set; }
+    public int Digit { get; private set; }
+
+    public Conta(string holder, long number, int digit) {
+        _holder = holder;
+        Number = number;
+        Digit = digit;
+    }
+
+    public string GetOlder() {
+        return _holder;
+    }
+    public void SetOlder(string holder) { 
+        if(holder != null & holder.Length > 1) {
+            this._holder = holder;
+        }
+    }
+}
